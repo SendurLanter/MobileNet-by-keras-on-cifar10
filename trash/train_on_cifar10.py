ff173f158e3997a03f0632bdee9f5c80402418cd
sSplendidSs <smyonlys@gmail.com>
2019-07-12 09:56:37 +0800

    fuc

--- trash/train_on_cifar10.py
+++ trash/train_on_cifar10.py
@@ -9,9 +9,11 @@ num_classes = 6
 epochs = 20
 
 X_train = np.empty((2054,384,512,3),dtype="float32")
-y_train=np.empty((2054,1),dtype="float32")
+#y_train=np.empty((2054,1),dtype="float32")
 X_test = np.empty((473,384,512,3),dtype="float32")
-y_test=np.empty((473,1),dtype="float32")
+#y_test=np.empty((473,1),dtype="float32")
+y_train=list()
+y_test=list()
 def load():
 	tra_i=0
 	tes_i=0
@@ -23,11 +25,13 @@ def load():
 		#print(img.shape)
 		if i <= 322:
 			X_train[i] = img
-			y_train[i] = [1]
+			#y_train[i] = [1]
+			y_train.append([1])
 		else:
 			X_test[tes_i] = img
-			y_test[tes_i] = [1]
+			#y_test[tes_i] = [1]
 			tes_i+=1
+			y_test.append([1])
 
 	datas = os.listdir("./glass")
 	total = len(datas)
@@ -35,11 +39,13 @@ def load():
 		img = cv2.imread(datas[i])
 		if i <= 401:
 			X_train[i+322] = img
-			y_train[i+322] = [2]			
+			#y_train[i+322] = [2]
+			y_train.append([2])			
 		else:
 			X_test[tes_i] = img
-			y_test[tes_i] = [2]
-			tes_i+=1			
+			#y_test[tes_i] = [2]
+			tes_i+=1	
+			y_test.append([2])		
 
 	datas = os.listdir("./metal")
 	total = len(datas)
@@ -47,10 +53,12 @@ def load():
 		img = cv2.imread(datas[i])
 		if i <= 328:
 			X_train[i+322+401] = img
-			y_train[i+322+401] = [3]		
+			#y_train[i+322+401] = [3]		
+			y_train.append([3])
 		else:
 			X_test[tes_i] = img
-			y_test[tes_i] = [3]
+			#y_test[tes_i] = [3]
+			y_test.append([3])
 			tes_i+=1			
 
 	datas = os.listdir("./paper")
@@ -59,10 +67,12 @@ def load():
 		img = cv2.imread(datas[i])
 		if i <= 475:
 			X_train[i+322+328+401] = img
-			y_train[i+322+328+401] = [4]			
+			#y_train[i+322+328+401] = [4]	
+			y_train.append([4])		
 		else:
 			X_test[tes_i] = img
-			y_test[tes_i] = [4]
+			#y_test[tes_i] = [4]
+			y_test.append([4])
 			tes_i+=1			
 
 	datas = os.listdir("./plastic")
@@ -71,10 +81,12 @@ def load():
 		img = cv2.imread(datas[i])
 		if i <= 386:
 			X_train[i+322+328+401+475] =img
-			y_train[i+322+328+401+475] = [5]			
+			#y_train[i+322+328+401+475] = [5]	
+			y_train.append([5])		
 		else:
 			X_test[tes_i] = img
-			y_test[tes_i] = [5]
+			#y_test[tes_i] = [5]
+			y_test.append([5])
 			tes_i+=1			
 
 	datas = os.listdir("./trash")
@@ -83,13 +95,15 @@ def load():
 		img = cv2.imread(datas[i])
 		if i <= 190:
 			X_train[i+322+328+401+475+386] =img
-			y_train[i+322+328+401+475+386] = [0]		
+			#y_train[i+322+328+401+475+386] = [0]	
+			y_train.append([0])	
 		else:
 			X_test[tes_i] = img
-			y_test[tes_i] = [0]
+			#y_test[tes_i] = [0]
+			y_test.append([0])
 			tes_i+=1
 	#print(X_train)
-	return (X_train,y_train) , (X_test,y_test)
+	return (X_train,np.array(y_train)) , (X_test,np.array(y_test))
 
 (x_train, y_train), (x_test, y_test) = load()
 print(x_train.shape)
@@ -100,6 +114,10 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 x_train /= 255
 x_test /= 255
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
 img_input = keras.layers.Input(shape=(384, 512, 3))
 model = MobileNet(input_tensor=img_input, classes=num_classes)
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
