326273dd4f6a306d322c0c792d35ac6e65fb4e21
sSplendidSs <smyonlys@gmail.com>
2019-09-21 18:18:08 +0800

    recall

--- keras_mobilenet_cifar10/model.py
+++ keras_mobilenet_cifar10/model.py
@@ -1,10 +1,11 @@
 from keras_applications.imagenet_utils import _obtain_input_shape
 from keras import backend as K
-from keras.layers import Input, Convolution2D,GlobalAveragePooling2D, Dense, BatchNormalization, Activation
+from keras.layers import Input, Convolution2D, \
+    GlobalAveragePooling2D, Dense, BatchNormalization, Activation
 from keras.models import Model
 from keras.engine.topology import get_source_inputs
 from depthwise_conv2d import DepthwiseConvolution2D
-def MobileNet(input_tensor=None, input_shape=None, alpha=1, shallow=True, classes=6):
+def MobileNet(input_tensor=None, input_shape=None, alpha=1, shallow=False, classes=1000):
     input_shape = _obtain_input_shape(input_shape,
                                       default_size=224,
                                       min_size=96,
@@ -21,49 +22,50 @@ def MobileNet(input_tensor=None, input_shape=None, alpha=1, shallow=True, classe
     x = Convolution2D(int(32 * alpha), (3, 3), strides=(2, 2), padding='same', use_bias=False)(img_input)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
+
     x = DepthwiseConvolution2D(int(32 * alpha), (3, 3), strides=(1, 1), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
-
     x = Convolution2D(int(64 * alpha), (1, 1), strides=(1, 1), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
+
     x = DepthwiseConvolution2D(int(64 * alpha), (3, 3), strides=(2, 2), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
-    
     x = Convolution2D(int(128 * alpha), (1, 1), strides=(1, 1), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
+
     x = DepthwiseConvolution2D(int(128 * alpha), (3, 3), strides=(1, 1), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
-
-    '''x = Convolution2D(int(128 * alpha), (1, 1), strides=(1, 1), padding='same', use_bias=False)(x)
+    x = Convolution2D(int(128 * alpha), (1, 1), strides=(1, 1), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
+
     x = DepthwiseConvolution2D(int(128 * alpha), (3, 3), strides=(2, 2), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)'''
-
+    x = Activation('relu')(x)
     x = Convolution2D(int(256 * alpha), (1, 1), strides=(1, 1), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
+
     x = DepthwiseConvolution2D(int(256 * alpha), (3, 3), strides=(1, 1), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
-    
-    '''x = Convolution2D(int(256 * alpha), (1, 1), strides=(1, 1), padding='same', use_bias=False)(x)
+    x = Convolution2D(int(256 * alpha), (1, 1), strides=(1, 1), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
+
     x = DepthwiseConvolution2D(int(256 * alpha), (3, 3), strides=(2, 2), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
     x = Convolution2D(int(512 * alpha), (1, 1), strides=(1, 1), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)'''
+    x = Activation('relu')(x)
 
-    '''if not shallow:
+    if not shallow:
         for _ in range(5):
             x = DepthwiseConvolution2D(int(512 * alpha), (3, 3), strides=(1, 1), padding='same', use_bias=False)(x)
             x = BatchNormalization()(x)
@@ -84,7 +86,7 @@ def MobileNet(input_tensor=None, input_shape=None, alpha=1, shallow=True, classe
     x = Activation('relu')(x)
     x = Convolution2D(int(1024 * alpha), (1, 1), strides=(1, 1), padding='same', use_bias=False)(x)
     x = BatchNormalization()(x)
-    x = Activation('relu')(x)'''
+    x = Activation('relu')(x)
 
     x = GlobalAveragePooling2D()(x)
     out = Dense(classes, activation='softmax')(x)
--- keras_mobilenet_cifar10/train_on_cifar10.py
+++ keras_mobilenet_cifar10/train_on_cifar10.py
@@ -1,62 +1,27 @@
-import sys, os,cv2
+import sys, os
+sys.path.insert(0, os.path.abspath('..'))
 from keras.callbacks import TensorBoard
 import keras
-import numpy as np
-from model import MobileNet
-from keras.preprocessing import image
-batch_size = 64
-num_classes = 6
+from keras.datasets import cifar10
+from mobilenet import MobileNet
+batch_size = 128
+num_classes = 10
 epochs = 20
-
-x_train = np.empty((2517,224,224,3),dtype="float32")
-x_test=list()
-y_train=list()
-y_test=list()
-def load():
-	tra_i=0
-	tes_i=0
-	datas = os.listdir('./')
-	total = len(datas)
-	#print(datas)
-	for e in datas:
-		img = cv2.imread(e)
-		if e[:5] == 'cardb':
-			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-			x_train[tra_i] = img
-			y_train.append([1])
-			tra_i+=1
-		if e[:5] == 'glass':
-			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-			x_train[tra_i] = img
-			y_train.append([2])
-			tra_i+=1
-		if e[:5] == 'metal':
-			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-			x_train[tra_i] = img
-			y_train.append([3])
-			tra_i+=1
-		if e[:5] == 'paper':
-			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-			x_train[tra_i] = img
-			y_train.append([4])
-			tra_i+=1
-		if e[:5] == 'plast':
-			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-			x_train[tra_i] = img
-			y_train.append([5])
-			tra_i+=1
-		if e[:5] == 'trash':
-			img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-			x_train[tra_i] = img
-			y_train.append([0])
-			tra_i+=1
-	return (x_train,np.array(y_train)) , (x_test,np.array(y_test))
-
-(x_train, y_train), (x_test, y_test) = load()
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
 x_train /= 255
-img_input = keras.layers.Input(shape=(224, 224, 3))
+x_test /= 255
+img_input = keras.layers.Input(shape=(32, 32, 3))
 model = MobileNet(input_tensor=img_input, classes=num_classes)
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 model.summary()
-model.compile(loss='categorical_crossentropy',optimizer = keras.optimizers.rmsprop(lr=0.0004, decay=5e-4),metrics=['accuracy'])
-model.fit(x_train, y_train, validation_split =0.2,batch_size = batch_size, epochs = epochs, verbose = 1,shuffle=True)
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test),
+          shuffle=True,
+          verbose=1,
+          callbacks=[TensorBoard(log_dir='./runs/keras',write_grads=True)])
